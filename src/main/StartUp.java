58ced1b372d16d3b1f6000aed41312124ec3b73a
Pandakidz2 <shuyelin06@gmail.com>
2021-07-09 01:18:18 -0600

    Update StartUp.java

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -18,16 +18,16 @@ public class StartUp{
         		(Integer) Settings.getSetting(new String[]{"Resolution", "Height"})
         		);
         
-//        Request.pull();
-//        ArrayList<Member> members = new ArrayList<Member>();
-//        
-//        /* Processing and creating so many member objects takes time, needless to say there's
-//        *  probably a faster way to do this; I'm looking into it.
-//        */
-//        for(int i=2; i<Request.getMaxRows()-1; i++){
-//            members.add(Request.member(i));
-//        }
-//        
+        Request.pull();
+        ArrayList<Member> members = new ArrayList<Member>();
+        
+        /* Processing and creating so many member objects takes time, needless to say there's
+        *  probably a faster way to do this; I'm looking into it.
+        */
+        for(int i=2; i<Request.getMaxRows()-1; i++){
+            members.add(Request.member(i));
+        }
+        
 //        frame.setUp(members);
         frame.setVisible(true);
 
